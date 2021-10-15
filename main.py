7e08bdd22eaa4c1ce8ab11219ccb975622bea3bb
CaedenZ <Fengye.16@ichat.sp.edu.sg>
2021-10-15 12:13:47 +0800

    p

--- main.py
+++ main.py
@@ -43,9 +43,11 @@ class Graph:
         coor_s = self.coord[str(start)]
         coor_e = self.coord[str(end)]
         coor_f = self.coord[str(final)]
-        dist_start = (coor_s[0] - coor_f[0]) ^ 2 + (coor_s[1] - coor_f[1]) ^ 2
-        dist_end = (coor_e[0] - coor_f[0]) ^ 2 + (coor_e[1] - coor_f[1]) ^ 2
-        dist_moved = math.sqrt(dist_end - dist_start)
+        dist_start = math.sqrt(
+            ((coor_s[0] - coor_f[0]) ** 2) + ((coor_s[1] - coor_f[1]) ** 2))
+        dist_end = math.sqrt(
+            ((coor_e[0] - coor_f[0]) ** 2) + ((coor_e[1] - coor_f[1]) ** 2))
+        dist_moved = dist_end - dist_start
         moved_per_energy = dist_moved / energy
         return moved_per_energy
 
@@ -55,45 +57,45 @@ class Graph:
         dist = (coor_s[0] - coor_e[0]) ^ 2 + (coor_s[1] - coor_e[1]) ^ 2
         return dist
 
-    def astar(self, start, end, dist_so_far, energy_so_far, path=[]):
-        if self.found:
-            return
-        if energy_so_far > 287932:
-            return
+    # def astar(self, start, end, dist_so_far, energy_so_far, path=[]):
+    #     if self.found:
+    #         return
+    #     if energy_so_far > 287932:
+    #         return
 
-        path = path + [start]
-        # print(energy_so_far)
-        if start == end:
-            path.append(path)
-            del path[-1]
-            print(path)
-            print("Shortest distance: ", dist_so_far)
-            print("Total energy cost: ", energy_so_far)
-            self.path = path
-            self.res_dist = dist_so_far
-            self.res_energy = energy_so_far
-            self.found = True
-            return
+    #     path = path + [start]
+    #     # print(energy_so_far)
+    #     if start == end:
+    #         path.append(path)
+    #         del path[-1]
+    #         print(path)
+    #         print("Shortest distance: ", dist_so_far)
+    #         print("Total energy cost: ", energy_so_far)
+    #         self.path = path
+    #         self.res_dist = dist_so_far
+    #         self.res_energy = energy_so_far
+    #         self.found = True
+    #         return
 
-        N = []
-        for neighbor in self.node[str(start)]:
-            N.append(neighbor)
+    #     N = []
+    #     for neighbor in self.node[str(start)]:
+    #         N.append(neighbor)
 
-        N.sort(key=lambda x: self.heuristic(
-            start, x, 50, self.cost[str(start) + "," + str(x)]), reverse=True)
+    #     N.sort(key=lambda x: self.heuristic(
+    #         start, x, 50, self.cost[str(start) + "," + str(x)]), reverse=True)
 
-        print(self.distance(
-            start, 50))
+    #     print(self.distance(
+    #         start, 50))
 
-        for neighbor in N:
-            neighbor = int(neighbor)
-            distance = self.weight[str(start) + "," + str(neighbor)]
-            energy = self.cost[str(start) + "," + str(neighbor)]
+    #     for neighbor in N:
+    #         neighbor = int(neighbor)
+    #         distance = self.weight[str(start) + "," + str(neighbor)]
+    #         energy = self.cost[str(start) + "," + str(neighbor)]
 
-            if neighbor not in path:
-                self.astar(neighbor, end, dist_so_far +
-                           distance, energy_so_far+energy, path)
-        return
+    #         if neighbor not in path:
+    #             self.astar(neighbor, end, dist_so_far +
+    #                        distance, energy_so_far+energy, path)
+    #     return
 
     def astarv2(self, start_vertex, end_vertex):
         D = {v+1: float('inf') for v in range(self.v)}
@@ -216,5 +218,5 @@ if val == "3":
     print("\nShortest distance: ", D[50])
     print("Total energy cost: ", E[50])
 
-if val == "3":
+if val == "4":
     g.astar(1, 50, 0, 0)
